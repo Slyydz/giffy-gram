1a49ffdb3ff61ec6a55a1774b53bdcd33dac8619
Brady Williams <williams.cole29@yahoo.com>
2021-09-02 16:02:39 -0500

    Added gif incorporation onto the dom

---
+++ src/scripts/feed/Post.js
@@ -0,0 +1,13 @@
+export const Post = (postObject) => {
+    return `
+      <section class="post">
+        <header>
+            <h2 class="post__title">${postObject.title}</h2>
+        </header>
+        <img class="post__image" src="${postObject.imageURL}" />
+        <h4 class="description">${postObject.description}</h4>
+        <p timestamp>${Date(postObject.timestamp)}</p>
+        <p class="userId">User Id: ${postObject.userId}</p> 
+      </section>
+    `
+  }
